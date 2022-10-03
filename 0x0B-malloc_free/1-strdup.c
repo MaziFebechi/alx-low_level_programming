34adcb6fdcd3cecb81a43f1156440bfab9d669a6
febechi <godsonsaviour7@gmail.com>
2022-10-03 07:58:13 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -57,13 +57,12 @@ char *_strdup(char *str)
 
 	size = _strlen(str) + 1;
 
-	dst = (char *) malloc(size *sizeof(char));
+	dst = (char *) malloc(size * sizeof(char));
 
 	if (dst == 0)
 	{
 		return (NULL);
 	}
-	
 	_strcpy(dst, str);
 	return (dst);
 }
