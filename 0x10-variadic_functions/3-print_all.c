f238fa16ae1da7210429b5d8869fd36a60ca6473
febechi <godsonsaviour7@gmail.com>
2022-10-13 17:58:21 -0700

    3print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,8 +28,8 @@ void print_all(const char * const format, ...)
 			case 'c':
 				printf("%c", va_arg(valist, int)), k = 1;
 				break;
-			case'i':
-				printf("%f", va_arg(valist, int)), k = 1;
+			case 'i':
+				printf("%d", va_arg(valist, int)), k = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(valist, double)), k = 1;
